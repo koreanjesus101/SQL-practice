5e7094a39b1df8c80b350abde73fbcc401c2a37d
Sean Sung-Won Lee <seanlee423@gmail.com>
2025-02-27 15:53:26 -0700

    Update 3. where clause.sql

--- 3. where clause.sql
+++ 3. where clause.sql
@@ -25,4 +25,10 @@ FROM employee_demographics
 WHERE birth_date LIKE '1989%'
 ;
 
+SELECT *
+FROM employee_demographics
+-- Will grab anyone with the name er in it
+WHERE birth_date LIKE '%ER%'
+;
+
 
