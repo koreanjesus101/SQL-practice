9fbae5c901d15ec00a15a975fcf8febcb44c4fcc
Sean Sung-Won Lee <seanlee423@gmail.com>
2025-03-05 20:34:14 -0700

    Update 7. Joining .sql

--- 7. Joining .sql
+++ 7. Joining .sql
@@ -13,6 +13,9 @@ FROM employee_demographics AS dem
 INNER JOIN employee_salary AS sal
 	ON dem.employee_id = sal.employee_id
 ; 
+-- The INNER JOIN keyword returns only rows with a match in both tables. 
+-- Which means that if you have a product with no CategoryID, or with a CategoryID that is not present in the Categories table, 
+-- that record would not be returned as a result.
 
 -- Outer Joins Left and Right JOIN
 SELECT *
@@ -20,6 +23,20 @@ FROM employee_demographics AS dem
 RIGHT JOIN employee_salary AS sal
 	ON dem.employee_id = sal.employee_id
 ; 
+-- The RIGHT JOIN keyword returns all records from the right table (table2), 
+-- and the matching records from the left table (table1). 
+-- The result is 0 records from the left side, if there is no match.
+
+
+SELECT *
+FROM employee_demographics AS dem 
+LEFT JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+; 
+-- The LEFT JOIN keyword returns all records from the left table (table1), 
+-- and the matching records from the right table (table2). 
+-- The result is 0 records from the right side, if there is no match.
+
 
 -- Self Join 
 SELECT emp1.employee_id AS emp_santa, 
@@ -32,6 +49,9 @@ FROM employee_salary emp1
 JOIN employee_salary emp2
 	ON emp1.employee_id + 1 = emp2.employee_id
 ;
+--A self join is a regular join, but the table is joined with itself.
+
+
 
 -- Joining multiple tables together 
 SELECT *
